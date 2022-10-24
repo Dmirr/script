1f3704cda1ad48cef4ac087c41097d2f6818b0cc
Dmirr <dmitriyrodionow@gmail.com>
2022-10-24 13:28:49 +0300

    adedd script

---
+++ script.js
@@ -0,0 +1,16 @@
+console.log("Hi, JS student!1");
+console.log("Hi, JS student!2");
+console.log("Hi, JS student!3");
+
+let userName = "Dmitriy";
+let anotherName = userName;
+console.log(userName); //comment
+console.log(anotherName);
+
+const BIRH_DAY = "20.06.1977";
+
+console.log(BIRH_DAY);
+
+let balance = 100;
+let penalty = balance > 0 ? 50 : 0;
+console.log(penalty);
