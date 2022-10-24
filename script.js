58497e3b3a4640a691ba2e07cc89475403736750
Dmirr <dmitriyrodionow@gmail.com>
2022-10-24 23:40:21 +0300

    aded js info

--- script.js
+++ script.js
@@ -11,6 +11,11 @@ const BIRH_DAY = "20.06.1977";
 
 console.log(BIRH_DAY);
 
+let x = 1;
+let y = 3;
+let c = y - x;
+console.log(c);
+
 let balance = 100;
 let penalty = balance > 0 ? 50 : 0;
 console.log(penalty);
@@ -20,3 +25,19 @@ let stringType = "text";
 let stringTypeanother = "anothertext";
 let stringType2 = 'text with "braces"';
 console.log(stringType2);
+
+let myName = "Dmitriy";
+let surName = "Rodionov";
+let fullName = myName + " " + surName;
+console.log("This is my fullname " + fullName);
+console.log(`This is my fullname ${fullName} \u270C.`);
+
+let anotherNumber = 5;
+console.log(typeof anotherNumber); //number
+anotherNumber = String(anotherNumber);
+console.log(typeof anotherNumber); //string
+
+let str = "abc";
+console.log(typeof str); //string
+str = Number(str);
+console.log(typeof str); //number
