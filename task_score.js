b99c5f15a540374c62223d4cebf3db6faf9d5bce
Dmirr <dmitriyrodionow@gmail.com>
2022-10-25 14:38:31 +0300

    added new script

---
+++ task_score.js
@@ -0,0 +1,13 @@
+let introModule = 100;
+let gitModule = 269;
+let jsModule = 175;
+let totalScore = introModule + gitModule + jsModule;
+console.log(totalScore);
+let averageScore = totalScore / 3;
+console.log("Average points for modules " + averageScore);
+
+let studentName = "Dmitriy";
+let studentSurname = "Rodionov";
+let studentFullname = studentName + " " + studentSurname;
+
+console.log(` Average points for ${studentFullname} is ${averageScore}`);
